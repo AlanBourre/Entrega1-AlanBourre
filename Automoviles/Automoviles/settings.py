22b6429a60279fa23ddf12f3fa088b83561a7fa9
AlanBourre <alanbourre9@hotmail.com>
2022-08-09 13:38:51 -0300

    agregando datos al settings para heroku

--- Automoviles/Automoviles/settings.py
+++ Automoviles/Automoviles/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,12 +78,16 @@ WSGI_APPLICATION = 'Automoviles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
